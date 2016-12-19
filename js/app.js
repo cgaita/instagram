96686c46a33b681ed06702a28c75218cd7d17fe8
cgaita <gaitacaroline@gmail.com>
2016-12-19 12:38:49 -0500

    js edit

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function assemble(data){
 	return "<li><h2 class='hidden'>caption</h2>"+ anchor +"</li>";
 };
 function app(){
-	$.get('http://beta.json-generator.com/api/json/get/VkxAzj8zG', function(res) {
+	$.get('https://beta.json-generator.com/api/json/get/VkxAzj8zG', function(res) {
 		// console.log(data)
 		var html = "";
 		res['data'].forEach(function(i){
